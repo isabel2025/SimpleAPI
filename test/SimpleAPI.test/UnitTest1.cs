551fc0b1d1f2d3dfff2559b1d25b7f0e346ec32a
Issabel <issabel@Issabels-MacBook-Air.local>
2023-06-08 11:49:33 +0000

    Initial Commit

---
+++ test/SimpleAPI.test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+namespace SimpleAPI.test
+{
+    public class UnitTest1
+    {
+         Mock<ILogger<WeatherForecastController>> mockLogger = new Mock<ILogger<WeatherForecastController>>();
+         [Fact]
+         public void GetReturnsMyName()
+         {
+             WeatherForecastController controller = new WeatherForecastController(mockLogger.Object);
+             var returnValue = controller.Get();
+             Assert.Equal("Isabel Prempeh", returnValue);
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
